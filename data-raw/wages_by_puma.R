73a9478299e06e597c38331b8eec46064d64f09c
camille-s <camille.seaberry@gmail.com>
2024-02-09 00:03:39 -0500

    Change levels of sex in wages_by_puma

--- data-raw/wages_by_puma.R
+++ data-raw/wages_by_puma.R
@@ -4,6 +4,9 @@ income_pumas <- readRDS(file.path("data-raw", "files", "income_by_puma.rds"))
 wages_by_puma <- income_pumas |>
   dplyr::bind_rows() |>
   dplyr::ungroup() |>
-  dplyr::select(-dplyr::matches("_se$"), -sample_n)
+  dplyr::select(-dplyr::matches("_se$"), -sample_n) |>
+  dplyr::mutate(sex = forcats::as_factor(sex)) |>
+  dplyr::mutate(sex = forcats::fct_recode(sex, Men = "Male", Women = "Female")) |>
+  dplyr::mutate(sex = forcats::fct_relevel(sex, "Men"))
 
 usethis::use_data(wages_by_puma, overwrite = TRUE)

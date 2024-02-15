afe7fc4b9f2e15d6ccdf4845058e457b822cf0a2
camille-s <camille.seaberry@gmail.com>
2024-02-15 12:30:36 -0500

    Add placeholders for percentage, dollar functions

---
+++ R/dollar.R
@@ -0,0 +1,14 @@
+# DOLLAR LABELING
+# Write a function that takes a number and returns a string formatted as a dollar amount
+# e.g. dollar(123) --> "$123"; dollar(1234) --> "$1,234"
+# Something you might want to decide is whether to round to the nearest dollar,
+# or include cents, or include an argument that lets the user decide.
+# e.g. should dollar(123.45) --> "$123", or "$123.45"? That's up to you.
+# How do you want to handle negative values? e.g. "-$123" vs "$-123"?
+# Look at the `format`, `paste`, and `round` functions, as you'll probably use
+# one or more of these, and you might want to mimic `round`'s arguments.
+# If you include additional arguments, put them after x.
+dollar <- function(x) {
+  # do some stuff to format x
+  x
+}
---
+++ R/percentage.R
@@ -0,0 +1,16 @@
+# PERCENTAGE LABELING
+# Write a function that takes a number and returns a string formatted as a percentage
+# e.g. percentage(0.12) --> "12%"; percentage(0.1) --> "10%"
+# Decide whether you want to only round to the nearest whole percentage,
+# or allow the user to set the number of decimal points they'd like?
+# How would you allow both
+#   percentage(0.123, mystery_argument) --> "12%" and
+#   percentage(0.123, mystery_argument) --> "12.3%"
+# Look at the `format`, `paste`, and `round` functions, as you'll probably use
+# one or more of these, and you might want to mimic `round`'s arguments.
+# Also look at functions in the `scales` package, which has something similar.
+# If you include additional arguments, put them after x.
+percentage <- function(x) {
+  # do some stuff to format x
+  x
+}
